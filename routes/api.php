c100eaac253c2eaebf285a40426952f281c89814
devealibarakat <Dev.ali.barakat@gmail.com>
2025-12-20 13:26:20 +0300

    Routes Api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+
+
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+    Route::post('logout',   [AuthController::class, 'logout']);
+    Route::get('users',     [AuthController::class, 'userList']);
+    Route::get('profile',   [AuthController::class, 'userProfile']);
+    Route::put('profile',   [AuthController::class, 'updateProfile']);
+    Route::delete('profile',[AuthController::class, 'destroy']);
+});
